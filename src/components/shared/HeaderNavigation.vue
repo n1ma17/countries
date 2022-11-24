48502dec49e78cf49171d188f6b25da4f937b91c
n1ma17 <nimaa9217@gmail.com>
2022-11-24 20:20:08 +0330

    wip: create header component

---
+++ src/components/shared/HeaderNavigation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header container">
+    <span class="header__title">Where in the world?</span>
+    <button>dark/light</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderNavigation",
+  props: {},
+};
+</script>
+
+<style scoped lang="scss">
+@import "@/assets/scss/mixins.scss";
+.header {
+  height: 80%;
+  @include flex(row, space-between, center);
+  &__title {
+    @include font(24px, 600);
+  }
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home container">home</div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  components: {},
+};
+</script>
+<style scoped lang="scss">
+.home {
+  height: calc(100% - 100px);
+  padding: 50px 0;
+}
+</style>
